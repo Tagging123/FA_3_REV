f06b15c3b4201de7216642df784a5c9768d73850
Tagging123 <125435770+Tagging123@users.noreply.github.com>
2023-02-22 09:58:42 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function resp(){
+    window.open('sign_in.html', '_self')
+}
+
+function show(){
+    alert("Thank You for asking :)")
+}
+
+function show_2(){
+    alert("Congrats, You have created your account :)")
+}
+
+function show_3(){
+    var value = document.getElementById("inputusername").value;
+    alert("Welcome back, "+value);
+}
+
+// function mySubmit(frm) {
+//     document.getElementById("message").style.display = 'block';
+//     return false;
+// }
